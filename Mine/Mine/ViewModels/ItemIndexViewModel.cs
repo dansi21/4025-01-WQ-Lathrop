0f185c1ab207277d3d8b724ae194c2e5c5471b2f
William Lathrop <willguy21@gmail.com>
2020-01-28 20:36:32 -0800

    Added Delete support to ItemIndexViewModel

--- Mine/Mine/ViewModels/ItemIndexViewModel.cs
+++ Mine/Mine/ViewModels/ItemIndexViewModel.cs
@@ -52,6 +52,10 @@ namespace Mine.ViewModels
             {
                 await Add(data as ItemModel);
             });
+            MessagingCenter.Subscribe<ItemDeletePage, ItemModel>(this, "Delete", async (obj, data) =>
+            {
+                await Delete(data as ItemModel);
+            });
         }
 
         /// <summary>
@@ -65,6 +69,17 @@ namespace Mine.ViewModels
             var result = await DataStore.CreateAsync(data);
 
             return true;
+        }
+        public async Task<bool> Delete(ItemModel data)
+        {
+
+            var record = await Read(data.Id);
+            if (record == null)
+                return false;
+            Dataset.Remove(data);
+            var result = await DataStore.DeleteAsync(data.Id);
+
+            return result;
         }
 
         #region Refresh
